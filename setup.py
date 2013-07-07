3fae107b1e39a7fb2ec08ba7f2221bdf115652b9
Nathaniel Tucker <me@ntucker.me>
2013-07-07 18:37:57 +0000

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='django-aloha-edit',
-      version='0.1.0',
+      version='0.1.1',
       description='Django Aloha Edit',
       author='Nathaniel Tucker',
       author_email='me@ntucker.me',
