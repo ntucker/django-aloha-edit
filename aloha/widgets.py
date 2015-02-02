6fa48c4f8915554dcba22b174ff0211b3d0f3e4f
Nathaniel Tucker <me@ntucker.me>
2015-02-02 06:31:45 +0000

    Django 1.7 deprecations

--- aloha/widgets.py
+++ aloha/widgets.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django import forms
 from django.utils.safestring import mark_safe
 from django.utils.encoding import force_text
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 
 
 class AlohaWidget(forms.Widget):
